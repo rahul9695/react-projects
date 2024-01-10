39398d429f7b2888ac39f0d76e8a3079d97ce116
Rahul Meena <rahul.meena9695@gmail.com>
2024-01-10 20:28:28 +0530

    UI updated

--- useref-password-generator/src/components/PasswordGenerator.jsx
+++ useref-password-generator/src/components/PasswordGenerator.jsx
@@ -1,19 +1,20 @@
 import { useState, useRef, useCallback } from "react";
+import lockImage from '../assets/security.png'
 
 const PasswordGenerator = () => {
   const [password, setPassword] = useState("");
   const [length, setLength] = useState(12);
-  const [includeNumbers, setIncludeNumbers] = useState(true);
-  const [includeCharacters, setIncludeCharacters] = useState(true);
+  const [includeNumbers, setIncludeNumbers] = useState(false);
+  const [includeCharacters, setIncludeCharacters] = useState(false);
 
   const passwordRef = useRef(null);
 
   const generatePassword = useCallback(() => {
-    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const numSet = "0123456789";
     const charSet = "!@#$%^&*()_-+=<>?";
 
-    let finalSet = charset;
+    let finalSet = alphabets;
 
     if (includeNumbers) {
       finalSet += numSet;
@@ -42,43 +43,51 @@ const PasswordGenerator = () => {
   };
 
   return (
-    <div>
-      <label>
-        Password Length:
-        <input
-          type="number"
-          value={length}
-          onChange={(e) => setLength(e.target.value)}
-          min={4}
-        />
-      </label>
-      <br />
-      <label>
-        Include Numbers:
-        <input
-          type="checkbox"
-          checked={includeNumbers}
-          onChange={() => setIncludeNumbers(!includeNumbers)}
-        />
-      </label>
-      <br />
-      <label>
-        Include Special Characters:
-        <input
-          type="checkbox"
-          checked={includeCharacters}
-          onChange={() => setIncludeCharacters(!includeCharacters)}
-        />
-      </label>
-      <br />
-      <button onClick={generatePassword}>Generate Password</button>
-      <br />
-      {password && (
-        <>
-          <textarea ref={passwordRef} value={password} readOnly />
-          <button onClick={copyToClipboard}>Copy to Clipboard</button>
-        </>
-      )}
+    <div className="appContainer">
+      <div>
+        <label>
+          Password Length:
+          <input
+            type="number"
+            value={length}
+            onChange={(e) => setLength(e.target.value)}
+            min={4}
+          />
+        </label>
+        <p className="byDefault">
+          By default upper and lower case alphabets are included
+        </p>
+        <br />
+        <label>
+          Include Numbers:
+          <input
+            type="checkbox"
+            checked={includeNumbers}
+            onChange={() => setIncludeNumbers(!includeNumbers)}
+          />
+        </label>
+        <br />
+        <label>
+          Include Special Characters:
+          <input
+            type="checkbox"
+            checked={includeCharacters}
+            onChange={() => setIncludeCharacters(!includeCharacters)}
+          />
+        </label>
+        <br />
+        <button onClick={generatePassword}>Generate Password</button>
+        <br />
+        {password && (
+          <>
+            <textarea ref={passwordRef} value={password} readOnly />
+            <button onClick={copyToClipboard}>Copy to Clipboard</button>
+          </>
+        )}
+      </div><div className="sidebarImg">
+        <img src={lockImage} alt="img" />
+      </div>
+      
     </div>
   );
 };
