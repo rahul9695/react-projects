8e3d200d570170ec920b94d3470dbcd52162cc2c
Rahul Meena <rahul.meena9695@gmail.com>
2024-01-10 18:33:25 +0530

    useref password generator project initial update

---
+++ useref-password-generator/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import PasswordGenerator from './components/PasswordGenerator.jsx';
+
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <PasswordGenerator />
+      </div>
+    </>
+  );
+}
+
+export default App;
